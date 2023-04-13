d5bb0ee7b80b397e433f9c0543c3ec6d021188b6
AnastasiaBogacheva <130046692+AnastasiaBogacheva@users.noreply.github.com>
2023-04-13 19:13:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,7 +8,8 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p1 = x_success/x_cnt
+    p2 = y_success/y_cnt
+    d = (p2-p1)/np.sqrt((p1 * (1 - p1) / x_cnt) + (p2 * (1-p2) / y_cnt))
+    d = d > norm.ppf(0.99)
+    return d
